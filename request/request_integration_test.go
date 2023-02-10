041beb19d79595cae4b337f1e483c48909c95b6e
Matthew A Elder <matthew@umee.cc>
2023-02-09 17:05:43 -0700

    refactor a bit more

--- request/request_integration_test.go
+++ request/request_integration_test.go
@@ -4,7 +4,6 @@ import (
 	"strings"
 
 	"github.com/dreamcodez/brodash/request"
-	"github.com/dreamcodez/brodash/result"
 	"github.com/go-resty/resty/v2"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -19,16 +18,17 @@ var _ = Describe("Request Integration", func() {
 				request.R("GET", "https://swapi.dev/api/planets/2"),
 				request.R("GET", "https://swapi.dev/api/planets/3"),
 			}
-			results := request.ParReq(reqs)
-			slices.SortFunc(results, func(a, b result.Result[*resty.Response]) bool {
-				return strings.Compare(a.Val.Request.URL, b.Val.Request.URL) == -1
+
+			results, err := request.ParReq(reqs).Get()
+			Expect(err).To(BeNil())
+			slices.SortFunc(results, func(a, b *resty.Response) bool {
+				return strings.Compare(a.Request.URL, b.Request.URL) == -1
 			})
-			Expect(results.Ok()).To(BeTrue())
+
 			Expect(len(results)).To(Equal(3))
-			vals := results.Values()
-			Expect(vals[0].Body()).To(ContainSubstring(`"name":"Tatooine"`))
-			Expect(vals[1].Body()).To(ContainSubstring(`"name":"Alderaan"`))
-			Expect(vals[2].Body()).To(ContainSubstring(`"name":"Yavin IV"`))
+			Expect(results[0].Body()).To(ContainSubstring(`"name":"Tatooine"`))
+			Expect(results[1].Body()).To(ContainSubstring(`"name":"Alderaan"`))
+			Expect(results[2].Body()).To(ContainSubstring(`"name":"Yavin IV"`))
 		})
 	})
 })
